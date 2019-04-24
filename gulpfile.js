fc5fae12b79d613a3f7e51df75c55178805d49d2
James Arnall <james@jamesarnall.com>
2019-04-24 16:29:18 -0500

    Gulp to v4, now just compiles SASS. Will add more useful stuff later

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var gulp        = require('gulp')
-,   browserSync = require('browser-sync').create()
+var { gulp, src, dest, watch, series, parallel } = require('gulp')
+,   browserSync = require('browser-sync')
 ,   concat      = require('gulp-concat')
 ,   sass        = require('gulp-sass')
 ,   uglify      = require('gulp-uglify')
@@ -10,82 +10,64 @@ var paths = {
     dest : './dist'
 };
 
-gulp.task('html', function() {
-    return gulp.src(paths.src + '/*.html')
-             .pipe(gulp.dest(paths.dest));
-});
+var fontsRoboto = function(done) {
+    return src('./node_modules/roboto-fontface/fonts/roboto/*.*')
+             .pipe(dest('./WeatherStation/wwwroot/fonts/roboto'));
+}
 
-gulp.task('img', function() {
-    return gulp.src(paths.src + '/img/**/*.*')
-             .pipe(gulp.dest(paths.dest + '/img'));
-});
+var fontsWeather = function(done) {
+    return src('./lib/weather-icons-master/font/*.*')
+             .pipe(dest('./WeatherStation/wwwroot/fonts/weather'));
+}
 
-gulp.task('fonts', ['fonts-roboto', 'fonts-weather'], function() { });
-
-gulp.task('fonts-roboto', function() {
-    return gulp.src('./node_modules/roboto-fontface/fonts/roboto/*.*')
-             .pipe(gulp.dest('./WeatherStation/wwwroot/fonts/roboto'));
-});
-
-gulp.task('fonts-weather', function() {
-    return gulp.src('./lib/weather-icons-master/font/*.*')
-             .pipe(gulp.dest('./WeatherStation/wwwroot/fonts/weather'));
-});
-
-gulp.task('sass', ['fonts'], function() {
-    return gulp.src('./WeatherStation/wwwroot/css/*.scss')
+var buildSass = function(done) {
+    return src('./WeatherStation/wwwroot/css/*.scss')
              .pipe(sass())
-             .pipe(gulp.dest('./WeatherStation/wwwroot/css'))
+             .pipe(dest('./WeatherStation/wwwroot/css'))
              .pipe(browserSync.stream());
            ;
-});
+}
 
-//browsersync
-gulp.task('browserSync', function () {
+var bSync = function(done) {
     var files = [
        './WeatherStation/wwwroot/css/*.scss'
     ];
 
     browserSync.init(files, {
-
         proxy: "http://localhost:5000/"
     });
-});
-
-gulp.task('js', function() {
-    return gulp.src([
-            './node_modules/jquery/dist/jquery.min.js', 
-            './node_modules/moment/min/moment.min.js', 
-            './WeatherStation/wwwroot/js/api.js', 
-            './WeatherStation/wwwroot/js/weatherapp.js'
-        ])
-        .pipe(concat('scripts.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./WeatherStation/wwwroot/js'));
-});
-
-gulp.task('build', ['html','img','sass', 'js'], function() {
+}
 
 
 
-});
+// //browsersync
+// gulp.task('browserSync', function () {
+//     var files = [
+//        './WeatherStation/wwwroot/css/*.scss'
+//     ];
+
+//     browserSync.init(files, {
+
+//         proxy: "http://localhost:5000/"
+//     });
+// });
+
+// gulp.task('js', function() {
+//     return gulp.src([
+//             './node_modules/jquery/dist/jquery.min.js', 
+//             './node_modules/moment/min/moment.min.js', 
+//             './WeatherStation/wwwroot/js/api.js', 
+//             './WeatherStation/wwwroot/js/weatherapp.js'
+//         ])
+//         .pipe(concat('scripts.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('./WeatherStation/wwwroot/js'));
+// });
+
+// gulp.task('build', ['html','img','sass', 'js'], function() {
 
 
-gulp.task('watch-sass', ['sass'], function() {
-    gulp.watch("./WeatherStation/wwwroot/css/*.scss", ['sass', browserSync.reload]);
-});
 
-// TODO: Out of date, using static files and we've moved on to .NET
-gulp.task('serve', ['build'], function() {
+// });
 
-    browserSync.init({
-        server: paths.dest
-    });
-
-    gulp.watch("app/sass/*.scss", ['sass']);
-    gulp.watch([paths.src + "/*.html", paths.src + "/js/**.*/*.js"], ['build','sass'])
-          .on('change', browserSync.reload);
-
-
-
-});
+exports.default = parallel(fontsRoboto, fontsWeather, buildSass);
