5782777e899e3483cdcd6ec852249a115b6d0639
James Arnall <james@jamesarnall.com>
2019-10-02 17:39:45 -0500

    Re-gulpifying this

--- gulpfile.js
+++ gulpfile.js
@@ -1,91 +1,43 @@
-var gulp        = require('gulp')
-,   browserSync = require('browser-sync').create()
-,   concat      = require('gulp-concat')
-,   sass        = require('gulp-sass')
-,   uglify      = require('gulp-uglify')
-;
+const { dest, parallel, series, src, watch }  = require('gulp');
+const browsersync = require('browser-sync').create();
+const concat      = require('gulp-concat');
+const del         = require('del');
+const sass        = require('gulp-sass');
+const uglify      = require('gulp-uglify');
 
-var paths = {
-    src  : './app',
-    dest : './dist'
+const files = {
+    css : 'WeatherStation/wwwroot/css/',
+    js  : 'WeatherStation/wwwroot/js/'
 };
 
-gulp.task('html', function() {
-    return gulp.src(paths.src + '/*.html')
-             .pipe(gulp.dest(paths.dest));
-});
+function cleanCss(done) {
+    del(files.css + 'styles.css');
+    done();
+}
 
-gulp.task('img', function() {
-    return gulp.src(paths.src + '/img/**/*.*')
-             .pipe(gulp.dest(paths.dest + '/img'));
-});
+function cleanJs(done) {
+    del(files.js + 'scripts.js');
+    done();
+}
 
-gulp.task('fonts', ['fonts-roboto', 'fonts-weather'], function() { });
+function css() {
+    return src(files.css + 'styles.scss')
+      .pipe(sass())
+      .pipe(dest(files.css));
+}
 
-gulp.task('fonts-roboto', function() {
-    return gulp.src('./node_modules/roboto-fontface/fonts/roboto/*.*')
-             .pipe(gulp.dest('./WeatherStation/wwwroot/fonts/roboto'));
-});
-
-gulp.task('fonts-weather', function() {
-    return gulp.src('./lib/weather-icons-master/font/*.*')
-             .pipe(gulp.dest('./WeatherStation/wwwroot/fonts/weather'));
-});
-
-gulp.task('sass', ['fonts'], function() {
-    return gulp.src('./WeatherStation/wwwroot/css/*.scss')
-             .pipe(sass())
-             .pipe(gulp.dest('./WeatherStation/wwwroot/css'))
-             .pipe(browserSync.stream());
-           ;
-});
-
-//browsersync
-gulp.task('browserSync', function () {
-    var files = [
-       './WeatherStation/wwwroot/css/*.scss'
-    ];
-
-    browserSync.init(files, {
-
-        proxy: "http://localhost:5000/"
-    });
-});
-
-gulp.task('js', function() {
-    return gulp.src([
-            './node_modules/jquery/dist/jquery.min.js', 
-            './node_modules/moment/min/moment.min.js', 
-            './WeatherStation/wwwroot/js/api.js', 
-            './WeatherStation/wwwroot/js/weatherapp.js'
-        ])
+function js() {
+    return src([
+                    './node_modules/jquery/dist/jquery.min.js', 
+                    './node_modules/moment/min/moment.min.js', 
+                    files.js + 'api.js', 
+                    files.js + 'weatherapp.js'
+                ])
         .pipe(concat('scripts.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./WeatherStation/wwwroot/js'));
-});
+        .pipe(dest(files.js));
+}
 
-gulp.task('build', ['html','img','sass', 'js'], function() {
-
-
-
-});
-
-
-gulp.task('watch-sass', ['sass'], function() {
-    gulp.watch("./WeatherStation/wwwroot/css/*.scss", ['sass', browserSync.reload]);
-});
-
-// TODO: Out of date, using static files and we've moved on to .NET
-gulp.task('serve', ['build'], function() {
-
-    browserSync.init({
-        server: paths.dest
-    });
-
-    gulp.watch("app/sass/*.scss", ['sass']);
-    gulp.watch([paths.src + "/*.html", paths.src + "/js/**.*/*.js"], ['build','sass'])
-          .on('change', browserSync.reload);
-
-
-
-});
+exports.css     = series(cleanCss, css);
+exports.js      = series(cleanJs, js);
+exports.default = series(parallel(cleanCss, cleanJs), parallel(css, js));
