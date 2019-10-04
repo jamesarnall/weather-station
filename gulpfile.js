267e0af33c5be2b135bf8814eaa61ceb6b43f232
James Arnall <james@jamesarnall.com>
2019-10-04 16:42:04 -0500

    Got BrowserSync working, along with CSS injection

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,17 @@ const concat      = require('gulp-concat');
 const del         = require('del');
 const sass        = require('gulp-sass');
 const uglify      = require('gulp-uglify');
+const { clean, restore, build, test, pack, publish, run } = require('gulp-dotnet-cli');
 
 const files = {
     css : 'WeatherStation/wwwroot/css/',
     js  : 'WeatherStation/wwwroot/js/'
 };
+
+function buildDotnet(done) {
+    return src('*.sln', {read: false})
+        .pipe(build());
+}
 
 function cleanCss(done) {
     del(files.css + 'styles.css');
@@ -23,7 +29,8 @@ function cleanJs(done) {
 function css() {
     return src(files.css + 'styles.scss')
       .pipe(sass())
-      .pipe(dest(files.css));
+      .pipe(dest(files.css))
+      .pipe(browsersync.stream());
 }
 
 function js() {
@@ -38,6 +45,35 @@ function js() {
         .pipe(dest(files.js));
 }
 
-exports.css     = series(cleanCss, css);
-exports.js      = series(cleanJs, js);
-exports.default = series(parallel(cleanCss, cleanJs), parallel(css, js));
+function runDotnet(done) {
+    src('WeatherStation/WeatherStation.csproj', {read: false})
+        .pipe(run());
+    done();
+}
+
+function serve(done) {
+    browsersync.init({
+        proxy: "localhost:5000",
+        files: ['WeatherStation/wwwroot/css/*.css']
+    });
+
+    watch([files.css + '*.scss'], css);
+    done();
+}
+
+
+exports.css         = series(cleanCss, css);
+exports.js          = series(cleanJs, js);
+exports.buildDotnet = series(buildDotnet);
+exports.runDotnet   = series(buildDotnet, runDotnet);
+exports.serve       = series(runDotnet, serve);
+exports.default     = series(parallel(cleanCss, cleanJs), parallel(css, js));
+
+/*
+
+TODO:
+ - Build CSS
+ - Build JS
+ - Build & run dotnet
+ - Run BrowserSync & watch
+ */
